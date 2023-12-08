2ce6bb6dda14c4322e15e0c127b6eab8a6cfb42b
Shadan Shamshad <shadanshamshad@gmail.com>
2023-12-08 21:17:16 +0530

    Condition & Loop Assignment

---
+++ Q4.js
@@ -0,0 +1,23 @@
+// Generate numbers between any 2 given numbers.
+
+
+
+let num1 = 20;
+let num2 = 30;
+
+for( let i = num1; i <= num2; i++ ){
+   console.log(i);
+}
+
+// Output
+// 20
+// 21
+// 22
+// 23
+// 24
+// 25
+// 26
+// 27
+// 28
+// 29
+// 30
---
+++ Q5.js
@@ -0,0 +1,36 @@
+// Use the while loop to print numbers from 1 to 25 in ascending and descending order
+
+
+let i = 1,  num = 25; 
+while (i <= num )  {     
+    console.log(i); 
+   i = i + 1; 
+  
+}   
+
+// OUTPUT
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+// 16
+// 17
+// 18
+// 19
+// 20
+// 21
+// 22
+// 23
+// 24
+// 25
